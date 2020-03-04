34ed9c6d9cb4faa8610303252953008b10080837
Roy Duineveld <royduineveld@gmail.com>
2020-03-04 10:16:15 +0100

    Bugfix

--- src/PostcodeNlServiceProvider.php
+++ src/PostcodeNlServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Speelpenning\PostcodeNl;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 use Speelpenning\PostcodeNl\Http\PostcodeNlClient;
 use Speelpenning\PostcodeNl\Services\AddressLookup;
@@ -18,7 +19,7 @@ class PostcodeNlServiceProvider extends ServiceProvider
             $this->getPathToConfigFile() => config_path('postcode-nl.php')
         ], 'config');
 
-        if (array_get($this->app['config'], 'postcode-nl.enableRoutes', false) and ! $this->app->routesAreCached()) {
+        if (Arr::get($this->app['config'], 'postcode-nl.enableRoutes', false) and ! $this->app->routesAreCached()) {
             require __DIR__ . '/Http/routes.php';
         }
     }
